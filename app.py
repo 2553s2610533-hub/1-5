cf516c5fcac40f1c25d646e0498b7cf46038f2ca
2553s2610533-hub <2553s2610533@g.cnees.kr>
2026-05-29 13:23:03 +0900

    Update Streamlit app title and add link button

--- app.py
+++ app.py
@@ -1,3 +1,17 @@
 import streamlit as st
-st.title('최립우 개잘생겻다')  
-st.write('최립우 사랑행')  
+st.titleimport streamlit as st
+
+st.set_page_config(
+    page_title="최립우 정보",
+    page_icon="📘"
+)
+
+st.title("최립우 나무위키")
+
+st.write("아래 버튼을 누르면 나무위키 페이지로 이동합니다.")
+
+st.link_button(
+    "최립우 나무위키 바로가기",
+    "https://namu.wiki/w/%EC%B5%9C%EB%A6%BD%EC%9A%B0"
+)
+st.write(
